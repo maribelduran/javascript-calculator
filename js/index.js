346a8b554659e0ac093cc6b165d456b845729a0d
Maribel Duran <maribelduranucm@gmail.com>
2017-01-30 22:24:18 -0800

    Implemented functionality for buttons 0-9.

--- js/index.js
+++ js/index.js
@@ -29,26 +29,78 @@ var calculator = {
 		this.currentEntry = 0;
 		return this.currentEntry;
 	},
-	updateCurrEntry: function(num){
-		if (this.previousEntry.length == 0){
-			this.currentEntry = "";
+	updateCurrEntry: function(val){
+
+		if (val == "+"){
+			if (this.curentEntry !== "+" || this.currentEntry !== "0"){
+				this.previousEntry.push(this.currenEntry);
+				this.currentEntry = "+";
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}else if (typeof val == 'number' && val !==0){
+			if (this.previousEntry.length == 0){
+				this.currentEntry = "";
+			}
+			var valToString = this.currentEntry.toString() + val.toString();
+			console.log("valToString is: " + valToString);
+			this.currentEntry = parseInt(valToString);
+			this.previousEntry.pop();
+			this.previousEntry.push(this.currentEntry);
+
 		}
-		this.currentEntry += num.toString();	
-		this.previousEntry.push(num.toString()); 
+		else if(val == 0){
+			if (this.previousEntry.length != 0){
+				var valToString = this.currentEntry.toString() + val.toString();
+				console.log("valToString is: " + valToString);
+				this.currentEntry = parseInt(valToString);
+				this.previousEntry.pop();
+				this.previousEntry.push(this.currentEntry);
+			}
+			//check that previousEntry is not empty or that we
+		}
+		else if(val == '.'){
+			//there can only be one decimal in the currenEntry
+		}
+		else if(val == "-"){
+			//check that previousEntry is not empty
+			//check that previousEntry is not another operator
+		}
+		else if(val == "*"){
+			//do something
+		}	
+		else if (val == "/"){
+			//do something
+		}
+		else if(val == "="){
+			//do something
+		}
+		else if (val =="+/-"){
+			//do something
+		}
+		else if (val == "allClear"){
+			this.currentEntry = "0";
+			this.previousEntry.splice(0,this.previousEntry.length);
+		}
+		console.log("previousEntry is: " + this.previousEntry[0]);
 	}
 };
 
 var controller = {
-	updateEntry: function(num){
-		console.log("adding " + num + " to current input value.");
-		calculator.updateCurrEntry(num);
+	updateEntry: function(val){
+		
+		console.log("Attempting to add " + val + " to current input value.");
+		calculator.updateCurrEntry(val);
 		view.displayEntry(calculator.currentEntry);
+		view.displayOperation(calculator.previousEntry);
 	}
 };
 
 var view = {
 	addEventListeners: function(){
-		// var 0 = document.getElementById("0");
+		 var btn_0 = document.getElementById("btn_0");
 		 var btn_1 = document.getElementById("btn_1");
 		 var btn_2 = document.getElementById("btn_2");
 		 var btn_3 = document.getElementById("btn_3");
@@ -58,7 +110,12 @@ var view = {
 		 var btn_7 = document.getElementById("btn_7");
 		 var btn_8 = document.getElementById("btn_8");
 		 var btn_9 = document.getElementById("btn_9");
+		 var btn_decimal = document.getElementById("btn_decimal");
+		 var btn_allClear = document.getElementById("btn_allClear");
 
+		 btn_0.addEventListener("click", function(){
+		 	controller.updateEntry(0);
+		 });
 		 btn_1.addEventListener("click", function(){
 		 	controller.updateEntry(1);
 		 });
@@ -86,15 +143,21 @@ var view = {
 		 btn_9.addEventListener("click", function(){
 		 	controller.updateEntry(9);
 		 });
+		 btn_add.addEventListener("click", function(){
+		 	controller.updateEntry("+");
+		 });
+		 btn_allClear.addEventListener("click", function(){
+		 	controller.updateEntry("allClear");
+		 });
 
 	},
 	displayEntry: function(val){
 		var entry = document.getElementById("entryField");
 		entry.innerHTML = val;
-		this.displayOperation;
 	},
 	displayOperation: function(val){
-
+		var operation = document.getElementById("operation");
+		operation.innerHTML = val;
 	}
 };
 
