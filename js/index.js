c76eac1c6034c4fcc7c519da167e275458c6139a
Maribel Duran <maribelduranucm@gmail.com>
2017-02-15 19:56:19 -0800

    Completed functionality for equals button.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
+//Still have 2 hours to catch up from last week.
+//7:15PM
+//Day 43& 44: Calculator is about 80% functional! Need to fix minor errors.
+
+
+//7:15PM - 
+//Day 45:
+//Able to recover after Error button.
+//
+//To fix:
+//How to recover after an "Error" output
+
+
+
+
 //view
 var calculator = {
-	total: 0,
+	total: '',
 	previousEntry: [],
 	currentEntry: "0",
 	currentOperation: "0",
@@ -10,6 +25,7 @@ var calculator = {
 	allClear: function(){
 		this.currentEntry = "0";
 		this.currentOperation = "0"
+		this.total = '';
 
 		this.previousEntry.splice(0, this.previousEntry.length);
 	},
@@ -27,7 +43,7 @@ var calculator = {
 			this.currentEntry = this.currentOperation = "0";
 
 		}else{
-			var mostRecentE = this.currentOperation[this.previousEntry.length-1];
+			var mostRecentE = this.currentOperation[this.currentOperation.length-1];
 			console.log("Entry being removed ", mostRecentE);
 			if ( /\*|\/|-|\+/.test(this.currentEntry)){
 				this.justClearedOprator = true;
@@ -42,17 +58,15 @@ var calculator = {
 			}
 			this.currentEntry = "0";
 			var entryArr = this.currentOperation.split(" ");
+			console.log(entryArr);
 			entryArr.splice(entryArr.length-1);
 			this.currentOperation = entryArr.join(" ");
-			console.log(this.currentOperation);
-			console.log(this.previousEntry);
 		}
 	},
 	updateCurrEntry: function(val){
 		var operators = /\*|\/|-|\+/;
 		var mostRecentEntry = this.previousEntry[this.previousEntry.length-1];
-		console.log("MostRecentEntry: " + mostRecentEntry);
-		console.log("CurrentEntry is: " + this.currentEntry);
+		var justCalculated =  /\=|Error/.test(this.currentOperation);
 
 		function isOperator(value){
 			var operators = /\*|\/|-|\+/;
@@ -60,8 +74,19 @@ var calculator = {
 		}
 
 		if (isOperator(val)){
-			if (isOperator(this.currentEntry) == false && this.justClearedNumber!= true) {
+
+			if (justCalculated){
+				this.currentEntry = val;
+				//this.currentOperation = this.total + val;
+				this.previousEntry.splice(0, this.previousEntry.length);
+				this.previousEntry.push(this.total);
+				this.updateCurrentOperation();
+				justCalculated = false;
+				console.log(justCalculated);
+
+			}else if (isOperator(this.currentEntry) == false && this.justClearedNumber!= true) {
 				if (this.currentEntry[this.currentEntry.length-1] == "."){
+					//remove trailing decimal values 
 					this.currentEntry = this.currentEntry.replace(/\./, "");
 				}
 				//if we haven't just cleared the current Entry
@@ -75,55 +100,70 @@ var calculator = {
 				this.updateCurrentOperation();
 			}
 			else if(isOperator(this.currentEntry)){
+				//replace operator
 				this.currentEntry = val;
 				this.updateCurrentOperation();
 			}	
+			console.log("end of code block");
 			
 		}else if (typeof val == 'number'){
-			if (this.currentEntry == "0"){
-				this.currentEntry = "";
-			}
-
-			if (this.justClearedNumber == true){
-					this.justClearedNumber = false;
+			this.justClearedNumber = false;
+			//might be able to use allClear() instead
+			if (this.currentEntry == "0" || (this.total!= "" && justCalculated) || this.currentEntry == "Error"){
+				this.currentEntry = this.total = "";
+				this.currentOperation = "";
+				this.previousEntry.splice(0, this.previousEntry.length)
 			}
 			
 			if (isOperator(this.currentEntry)){
 				this.previousEntry.push(this.currentEntry);
 				this.currentEntry = val.toString();
 				this.updateCurrentOperation();
+
 			}else if ((/\d/.test(mostRecentEntry) == true) && this.previousEntry.length > 0){
 				this.currentEntry = "0";
 			}else{ //concatenating numbers
 				var valToString = this.currentEntry.toString() + val.toString();
 				this.currentEntry = valToString;
 				this.updateCurrentOperation();
+				
 			}
 		}else if(val == '.'){
 			//check that decimal character is not in the currentEntry input value
 			//there can only be one decimal in the currenEntry
-			var contains_decimal = /\./.test(this.currentEntry);
 
+			if (justCalculated){
+				//Want to clear out currentOperation, currentEntry, and previousEntry
+			}
+
+			var contains_decimal = /\./.test(this.currentEntry);
 			if (!contains_decimal && this.justClearedOperator!= true){
 				if (isOperator(this.currentEntry)){
 				this.previousEntry.push(this.currentEntry);
 				this.currentEntry = "0";
 				this.updateCurrentOperation();
+				justCalculated = false;
 			}
 				var valToString = this.currentEntry.toString() + val.toString();
 				this.currentEntry = valToString;
 				this.updateCurrentOperation();
 			}	
-		}else if(val == "="){
+		}else if(val == "=" && !justCalculated){
 			if (this.previousEntry.length == 0){
+				//this.currentEntry = math.eval(this.currentOperation);
 				this.total = this.currentEntry;
+				this.currentOperation = this.currentEntry + "=" + this.currentEntry;
 
 			//if end of expression does not end with an operator.
-			}else if ((this.currentOperation.split(" ").length % 2) != 0){
-				//this.currentEntry = this.getTotal(this.currentOperation.split(" "));
-				this.currentEntry = math.eval(this.currentOperation);
+			}else if (!isOperator(this.currentOperation[this.currentOperation.length-1])){
+				this.total = math.eval(this.currentOperation);
+				this.currentEntry = this.total;
+				this.currentOperation += " = " + this.total;
+
 			}else{
-				this.currentEntry = "Error Unexpected end of expression";
+				this.currentEntry = "Error";
+				this.currentOperation = "Error";
+				this.total = "0";
 			}
 
 		}
@@ -131,7 +171,16 @@ var calculator = {
 			this.allClear();
 		}
 		else if (val == "clearEntry"){
-			this.clearEntry();
+			if (!justCalculated){
+				console.log("Just clearing entry");
+				this.clearEntry();
+
+
+			}
+			else{
+				this.allClear();
+			}
+			
 		}
 	},
 	//Figure out a better way to update the currentOperation
